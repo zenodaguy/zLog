746320aae2706b10b112589afa1cf1ace3bf6ce2
zeno <johnsmith444OFF@gmail.com>
2026-07-24 02:33:17 -0400

    added categories and category color showcase
    Updated logging calls to include categories and added success message logging.

--- example/main.cpp
+++ example/main.cpp
@@ -4,9 +4,17 @@ int main()
 {
     zLog::EnableTimestamp(true);
 
-    zLog::Info("info message");
-    zLog::Warning("warning message");
-    zLog::Error("error message");
+    zLog::Info("info message", "no color category");
+    zLog::Warning("warning message", "no color category");
+    zLog::Error("error message", "no color category");
+    zLog::Success("success message", "no color category");
+
+    zLog::EnableCategoryColors(false);
+
+    zLog::Info("info message", "color category");
+    zLog::Warning("warning message", "color category");
+    zLog::Error("error message", "color category");
+    zLog::Success("success message", "color category");
 
     return 0;
 }
